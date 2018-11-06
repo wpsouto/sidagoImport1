c973dffa169a69db9ded536fb35a61db11384bc6
Wemerson Souto <wemerson.souto@agrodefesa.go.gov.br>
2018-11-06 09:36:14 -0200

    First commit update

--- src/main/java/com/mycompany/myapp/domain/Pessoa.java
+++ src/main/java/com/mycompany/myapp/domain/Pessoa.java
@@ -7,15 +7,19 @@ import java.io.Serializable;
 /**
  * A Gta.
  */
+@Entity
+@Table(name = "pessoa", schema = "rh")
 public class Pessoa implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "nome")
     private String nome;
-
-    private String documento;
 
     // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
     public Integer getId() {
@@ -37,14 +41,6 @@ public class Pessoa implements Serializable {
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-    public void setDocumento(String documento) {
-        this.documento = documento;
     }
 
     @Override
