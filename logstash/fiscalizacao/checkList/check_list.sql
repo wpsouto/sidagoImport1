89f4efb386a182ff08c8fb16cd1056a7a80dcf82
Wemerson Souto <wemerson.souto@agrodefesa.go.gov.br>
2019-09-04 16:04:39 -0300

    Change

--- logstash/fiscalizacao/checkList/check_list.sql
+++ logstash/fiscalizacao/checkList/check_list.sql
@@ -1,32 +1,35 @@
-SELECT DISTINCT (tf.id_entidade || '-' || ch.id_checklist)       AS id,
-                case when ch.bo_ativo then 'Não' else 'Sim' end  AS cancelada,
-                pro.ds_programa                                  AS programa,
+SELECT DISTINCT (tf.id_entidade || '-' || ch.id_checklist)                               AS id,
+                case when ch.bo_ativo then 'Não' else 'Sim' end                          AS cancelada,
+                pro.ds_programa                                                          AS programa,
                 CASE
                     WHEN ch.tp_identificacao = 'bo_propriedade' THEN 'Propriedade'
                     WHEN ch.tp_identificacao = 'bo_empresa' THEN 'Empresa'
                     WHEN ch.tp_identificacao = 'bo_industria' THEN 'Industria'
                     WHEN ch.tp_identificacao = 'bo_evento' THEN 'Evento'
                     ELSE 'Não identificado'
-                    END                                            AS identificacao,
+                    END                                                                  AS identificacao,
 
-                ch.id_checklist                                  AS check_list_id,
-                ch.ds_descritivo                                 AS check_list_descricao,
+                CASE WHEN ultimo.quantidade > 1 THEN 'Sim' ELSE 'Não' end                AS revisitada,
 
-                ie.nu_inscricaoestadual                          AS fiscalizado_ie,
-                UPPER(COALESCE(ie.no_fantasia, tf.ds_nomerazao)) AS fiscalizado_nome,
-                mf_lr.nome                                       AS fiscalizado_regional_nome,
-                mf.loc_no                                        AS fiscalizado_municipio_nome,
-                mf.ufe_sg                                        AS fiscalizado_municipio_uf,
-                COALESCE(mf.lat, 0)                              AS fiscalizado_municipio_localizacao_latitude,
-                COALESCE(mf.lon, 0)                              AS fiscalizado_municipio_localizacao_longitude,
+                ch.id_checklist                                                          AS check_list_id,
+                ch.ds_descritivo                                                         AS check_list_descricao,
 
-                UPPER(pse.nome)                                  AS emissor_nome,
-                dse.numero                                       AS emissor_documento,
+                COALESCE(ie.nu_inscricaoestadual, CAST(ie.id_inscricaoestadual AS TEXT)) AS fiscalizado_ie,
+                UPPER(COALESCE(ie.no_fantasia, tf.ds_nomerazao))                         AS fiscalizado_nome,
+                mf_lr.nome                                                               AS fiscalizado_regional_nome,
+                mf.loc_no                                                                AS fiscalizado_municipio_nome,
+                mf.ufe_sg                                                                AS fiscalizado_municipio_uf,
+                COALESCE(mf.lat, 0)                                                      AS fiscalizado_municipio_localizacao_latitude,
+                COALESCE(mf.lon, 0)                                                      AS fiscalizado_municipio_localizacao_longitude,
 
-                tf.id_termofiscalizacao                          AS termo_fiscalizacao_id,
-                tf.id_entidade                                   AS termo_fiscalizacao_entidade_id,
-                tf.ds_rginscricaoestadual                        AS termo_fiscalizacao_entidade_documento,
-                tf.dt_criacaotermo                               AS termo_fiscalizacao_emissao
+                UPPER(pse.nome)                                                          AS emissor_nome,
+                dse.numero                                                               AS emissor_documento,
+
+                tf.id_termofiscalizacao                                                  AS termo_fiscalizacao_id,
+                tf.id_entidade                                                           AS termo_fiscalizacao_entidade_id,
+                tf.ds_rginscricaoestadual                                                AS termo_fiscalizacao_entidade_documento,
+                tf.dt_criacaotermo                                                       AS termo_fiscalizacao_emissao,
+                ultimo.quantidade                                                        AS termo_fiscalizacao_quantidade
 
 FROM fisc.checklist AS ch
          INNER JOIN fisc.programa_fiscalizacao AS pro ON ch.id_programafiscalizacao = pro.id_programafiscalizacao
@@ -45,11 +48,18 @@ FROM fisc.checklist AS ch
          LEFT JOIN rh.pessoa AS pse ON pse.id = tf.id_criadortermo
          LEFT JOIN rh.documento AS dse ON dse.id_pessoa = pse.id and dse.id_documento_tipo in (1, 2)
          INNER JOIN
-         (SELECT max(tf.dt_criacaotermo) as data, tf.id_entidade
-          FROM fisc.checklistresposta_tf AS chtf
+         (SELECT max(tf.dt_criacaotermo)                   as data,
+                 count(distinct (tf.id_termofiscalizacao)) AS quantidade,
+                 tf.id_inscricaoestadual                   AS inscricaoestadual_id,
+                 ch.id_checklist                           AS check_list_id
+          FROM fisc.checklist AS ch
+                   INNER JOIN fisc.checklist_pergunta AS cp ON cp.id_checklist = ch.id_checklist
+                   INNER JOIN fisc.checklist_resposta AS cr ON cr.id_checklistpergunta = cp.id_checklistpergunta
+                   INNER JOIN fisc.checklistresposta_tf AS chtf ON chtf.id_checklistresposta = cr.id_checklistresposta
                    INNER JOIN fisc.termo_fiscalizacao AS tf ON tf.id_termofiscalizacao = chtf.id_termofiscalizacao
           WHERE tf.ativo = true
-          GROUP BY tf.id_entidade) as ultimo ON ultimo.data = tf.dt_criacaotermo AND ultimo.id_entidade = tf.id_entidade
+          GROUP BY ch.id_checklist, tf.id_inscricaoestadual) as ultimo
+         ON ultimo.data = tf.dt_criacaotermo AND ultimo.check_list_id = ch.id_checklist AND ultimo.inscricaoestadual_id = tf.id_inscricaoestadual
 
 WHERE cp.id_checklist in (12, 18)
 ORDER BY tf.id_entidade
--- logstash/fiscalizacao/checkList/check_list_itens.sql
+++ logstash/fiscalizacao/checkList/check_list_itens.sql
@@ -1,35 +1,35 @@
-SELECT chtf.id_checklistrespostatf                                  AS id,
-       case when ch.bo_ativo then 'Não' else 'Sim' end              AS cancelada,
-       pro.ds_programa                                              AS programa,
-       tf.ts_alteracao                                              AS ts_alteracao,
+SELECT chtf.id_checklistrespostatf                                              AS id,
+       case when ch.bo_ativo then 'Não' else 'Sim' end                          AS cancelada,
+       pro.ds_programa                                                          AS programa,
+       tf.ts_alteracao                                                          AS ts_alteracao,
        CASE
            WHEN ch.tp_identificacao = 'bo_propriedade' THEN 'Propriedade'
            WHEN ch.tp_identificacao = 'bo_empresa' THEN 'Empresa'
            WHEN ch.tp_identificacao = 'bo_industria' THEN 'Industria'
            WHEN ch.tp_identificacao = 'bo_evento' THEN 'Evento'
            ELSE 'Não identificado'
-           END                                                      AS identificacao,
-       ch.id_checklist                                              AS check_list_id,
-       ch.ds_descritivo                                             AS check_list_descricao,
-       cp.ds_pergunta                                               AS pergunta,
-       TRIM(UPPER(cr.ds_resposta))                                  AS resposta,
-       cr.nu_pontuacao                                              AS pontuacao,
-       ie.nu_inscricaoestadual                                      AS fiscalizado_ie,
-       UPPER(COALESCE(ie.no_fantasia, tf.ds_nomerazao))             AS fiscalizado_nome,
-       mf_lr.nome                                                   AS fiscalizado_regional_nome,
-       mf.loc_no                                                    AS fiscalizado_municipio_nome,
-       mf.ufe_sg                                                    AS fiscalizado_municipio_uf,
-       COALESCE(mf.lat, 0)                                          AS fiscalizado_municipio_localizacao_latitude,
-       COALESCE(mf.lon, 0)                                          AS fiscalizado_municipio_localizacao_longitude,
+           END                                                                  AS identificacao,
+       ch.id_checklist                                                          AS check_list_id,
+       ch.ds_descritivo                                                         AS check_list_descricao,
+       cp.ds_pergunta                                                           AS pergunta,
+       TRIM(UPPER(cr.ds_resposta))                                              AS resposta,
+       cr.nu_pontuacao                                                          AS pontuacao,
+       COALESCE(ie.nu_inscricaoestadual, CAST(ie.id_inscricaoestadual AS TEXT)) AS fiscalizado_ie,
+       UPPER(COALESCE(ie.no_fantasia, tf.ds_nomerazao))                         AS fiscalizado_nome,
+       mf_lr.nome                                                               AS fiscalizado_regional_nome,
+       mf.loc_no                                                                AS fiscalizado_municipio_nome,
+       mf.ufe_sg                                                                AS fiscalizado_municipio_uf,
+       COALESCE(mf.lat, 0)                                                      AS fiscalizado_municipio_localizacao_latitude,
+       COALESCE(mf.lon, 0)                                                      AS fiscalizado_municipio_localizacao_longitude,
 
-       UPPER(pse.nome)                                              AS emissor_nome,
-       dse.numero                                                   AS emissor_documento,
+       UPPER(pse.nome)                                                          AS emissor_nome,
+       dse.numero                                                               AS emissor_documento,
 
-       tf.id_termofiscalizacao                                      AS termo_fiscalizacao_id,
-       tf.id_entidade                                               AS termo_fiscalizacao_entidade_id,
-       tf.ds_rginscricaoestadual                                    AS termo_fiscalizacao_entidade_documento,
-       tf.dt_criacaotermo                                           AS termo_fiscalizacao_emissao,
-       case when tf.ativo then 'Não' else 'Sim' end                 AS termo_fiscalizacao_cancelada
+       tf.id_termofiscalizacao                                                  AS termo_fiscalizacao_id,
+       tf.id_entidade                                                           AS termo_fiscalizacao_entidade_id,
+       tf.ds_rginscricaoestadual                                                AS termo_fiscalizacao_entidade_documento,
+       tf.dt_criacaotermo                                                       AS termo_fiscalizacao_emissao,
+       case when tf.ativo then 'Não' else 'Sim' end                             AS termo_fiscalizacao_cancelada
 FROM fisc.checklist AS ch
          INNER JOIN fisc.programa_fiscalizacao AS pro ON ch.id_programafiscalizacao = pro.id_programafiscalizacao
          INNER JOIN fisc.checklist_pergunta AS cp ON cp.id_checklist = ch.id_checklist
@@ -46,6 +46,6 @@ FROM fisc.checklist AS ch
                       mf_lr.bo_organograma = true
          LEFT JOIN rh.pessoa AS pse ON pse.id = tf.id_criadortermo
          LEFT JOIN rh.documento AS dse ON dse.id_pessoa = pse.id and dse.id_documento_tipo in (1, 2)
-WHERE ch.id_checklist in (12,18)
---AND tf.ts_alteracao > :sql_last_value
+WHERE ch.id_checklist in (12, 18)
+  AND tf.ts_alteracao > :sql_last_value
 ORDER BY chtf.id_checklistrespostatf
