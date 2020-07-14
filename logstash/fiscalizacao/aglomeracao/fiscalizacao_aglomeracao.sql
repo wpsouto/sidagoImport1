382ca41b57297329c276180cec6af0526fb74e3c
Wemerson Souto <wemerson.souto@agrodefesa.go.gov.br>
2020-07-14 15:55:47 -0300

    Correções

--- logstash/fiscalizacao/aglomeracao/fiscalizacao_aglomeracao.sql
+++ logstash/fiscalizacao/aglomeracao/fiscalizacao_aglomeracao.sql
@@ -1,55 +1,57 @@
-SELECT ac.id_aglomeracaociclo                          AS id,
-       ac.nu_evento                                    AS numero,
-       ac.ts_inicio                                    AS inicio,
-       ac.ts_fim                                       AS fim,
-       ac.ts_cadastro                                  AS cadastro,
-       c.ds_classificacao                              AS classificacao,
-       case when ac.bo_ativo then 'Não' else 'Sim' end AS cancelada,
+SELECT ac.id_aglomeracaociclo                                          AS id,
+       ac.nu_evento                                                    AS numero,
+       ac.ts_inicio                                                    AS inicio,
+       ac.ts_fim                                                       AS fim,
+       ac.ts_cadastro                                                  AS cadastro,
+       c.ds_classificacao                                              AS classificacao,
+       case when ac.bo_ativo then 'Não' else 'Sim' end                 AS cancelada,
 
        (SELECT case when COUNT(tf.id_termofiscalizacao) != 0 then 'Sim' else 'Não' end
         FROM fisc.termo_fiscalizacao AS tf
         WHERE ie.id_pessoa = tf.id_entidade
           and (tf.dt_criacaotermo BETWEEN ac.ts_inicio and ac.ts_fim)
-          and tf.bo_evento = true)                     AS vistoriado,
+          and tf.bo_evento = true)                                     AS vistoriado,
 
        (SELECT COALESCE(SUM(tro.nu_quantidade), 0)
         FROM fisc.termo_fiscalizacao AS tf
                  INNER JOIN fisc.termoobjetivo_fiscalizacao AS tro ON tf.id_termofiscalizacao = tro.id_termofiscalizacao
         WHERE ie.id_pessoa = tf.id_entidade
           and (tf.dt_criacaotermo BETWEEN ac.ts_inicio and ac.ts_fim)
-          and tf.bo_evento = true)                     AS animal_vistoriado_total,
+          and tf.bo_evento = true)                                     AS animal_vistoriado_total,
 
 
-       COALESCE(aet.total, 0)                          AS animal_entrada_total,
-       COALESCE(act.total, 0)                          AS animal_confirmado_total,
+       COALESCE(aet.total, 0)                                          AS animal_entrada_total,
+       COALESCE(act.total, 0)                                          AS animal_confirmado_total,
 
-       COALESCE(aebo.total, 0)                         AS animal_entrada_bovideo,
-       COALESCE(aee.total, 0)                          AS animal_entrada_equideo,
-       COALESCE(aeco.total, 0)                         AS animal_entrada_caprino_ovino,
+       COALESCE(aebo.total, 0)                                         AS animal_entrada_bovideo,
+       COALESCE(aee.total, 0)                                          AS animal_entrada_equideo,
+       COALESCE(aeco.total, 0)                                         AS animal_entrada_caprino_ovino,
 
-       ie.id_inscricaoestadual                         AS fiscalizado_id,
-       ie.no_fantasia                                  AS fiscalizado_nome,
-       ie.nu_inscricaoestadual                         AS fiscalizado_ie,
-       COALESCE(ie.vl_latitude, 0)                     AS fiscalizado_gps_latitude,
-       COALESCE(ie.vl_longitude, 0)                    AS fiscalizado_gps_longitude,
-       ll.cod_ibge                                     AS fiscalizado_municipio_ibge,
-       ll.loc_no                                       AS fiscalizado_municipio_nome,
-       ll.ufe_sg                                       AS fiscalizado_municipio_uf,
-       COALESCE(ll.lat, 0)                             AS fiscalizado_municipio_localizacao_latitude,
-       COALESCE(ll.lon, 0)                             AS fiscalizado_municipio_localizacao_longitude,
-       pai.nome                                        AS fiscalizado_regional_nome
+       case when COALESCE(aebo.total, 0) > 0 then 'Sim' else 'Não' end AS bovideo_presente,
+       case when COALESCE(aee.total, 0) > 0 then 'Sim' else 'Não' end  AS equideo_presente,
+       case when COALESCE(aeco.total, 0) > 0 then 'Sim' else 'Não' end AS caprino_presente,
+
+       ie.id_inscricaoestadual                                         AS fiscalizado_id,
+       ie.no_fantasia                                                  AS fiscalizado_nome,
+       ie.nu_inscricaoestadual                                         AS fiscalizado_ie,
+       COALESCE(ie.vl_latitude, 0)                                     AS fiscalizado_gps_latitude,
+       COALESCE(ie.vl_longitude, 0)                                    AS fiscalizado_gps_longitude,
+       ll.cod_ibge                                                     AS fiscalizado_municipio_ibge,
+       ll.loc_no                                                       AS fiscalizado_municipio_nome,
+       ll.ufe_sg                                                       AS fiscalizado_municipio_uf,
+       COALESCE(ll.lat, 0)                                             AS fiscalizado_municipio_localizacao_latitude,
+       COALESCE(ll.lon, 0)                                             AS fiscalizado_municipio_localizacao_longitude,
+       pai.nome                                                        AS fiscalizado_regional_nome
 
 FROM agrocomum.inscricaoestadual AS ie
          INNER JOIN agrocomum.inscricaoestadual_endereco AS iee ON ie.id_inscricaoestadual = iee.id_inscricaoestadual
          INNER JOIN agrocomum.endereco AS e ON iee.id_endereco = e.id_endereco
          INNER JOIN dsa.aglomeracao_ciclo AS ac ON ac.id_inscricaoestadual = ie.id_inscricaoestadual
          INNER JOIN agrocomum.classificacao AS c ON ie.id_classificacao = c.id_classificacao
-         INNER JOIN agrocomum.classificacao_macro_composta AS cmc
-                    ON cmc.id_classificacaooriginal = c.id_classificacao AND cmc.id_classificacaomacro = 5
+         INNER JOIN agrocomum.classificacao_macro_composta AS cmc ON cmc.id_classificacaooriginal = c.id_classificacao AND cmc.id_classificacaomacro = 5
          INNER JOIN dne.log_localidade AS ll ON e.id_localidade = ll.loc_nu
-         INNER JOIN rh.lotacao AS l
-                    ON l.id_localidade = ac.id_localidade and l.bo_ativo = true and l.bo_organograma = true and id_lotacaotipo = 3
-         INNER JOIN rh.lotacao AS pai ON pai.id = l.id_lotacao_pai
+         INNER JOIN rh.lotacao AS l ON l.id_localidade = ac.id_localidade AND l.id_lotacaotipo = 3 AND l.bo_ativo = true AND l.bo_organograma = true --Unidade Local
+         INNER JOIN rh.lotacao AS pai ON pai.id = l.id_lotacao_pai AND pai.id_lotacaotipo = 2 AND pai.bo_ativo = true AND pai.bo_organograma = true --Unidade Regional
 
          LEFT JOIN (SELECT ac.id_aglomeracaociclo AS id,
                            SUM(gem.nu_quantidade) AS total
@@ -81,9 +83,10 @@ FROM agrocomum.inscricaoestadual AS ie
                              INNER JOIN gta.gta AS gta ON gta.id_gta = gt.id_gta
                              INNER JOIN gta.estratificacao_gta AS gem ON gem.id_gta = gt.id_gta
                              INNER JOIN dsa.estratificacao AS es ON gem.id_estratificacao = es.id
+                             INNER JOIN dsa.especie AS esp ON esp.id = es.id_especie
                     WHERE gt.id_destinovinculado = ac.id_aglomeracaociclo
                       AND gt.tp_destino = 'aglomeracao'
-                      AND es.id_especie IN (1, 2) --Bovídeos
+                      AND esp.id_grupo = 1 --Bovídeos
                       AND gta.bo_ativo = TRUE
                     GROUP BY ac.id_aglomeracaociclo) AS aebo ON aebo.id = ac.id_aglomeracaociclo
 
@@ -94,9 +97,10 @@ FROM agrocomum.inscricaoestadual AS ie
                              INNER JOIN gta.gta AS gta ON gta.id_gta = gt.id_gta
                              INNER JOIN gta.estratificacao_gta AS gem ON gem.id_gta = gt.id_gta
                              INNER JOIN dsa.estratificacao AS es ON gem.id_estratificacao = es.id
+                             INNER JOIN dsa.especie AS esp ON esp.id = es.id_especie
                     WHERE gt.id_destinovinculado = ac.id_aglomeracaociclo
                       AND gt.tp_destino = 'aglomeracao'
-                      AND es.id_especie in (5, 6, 7)--Equídeos
+                      AND esp.id_grupo = 2 --Equídeos
                       AND gta.bo_ativo = TRUE
                     GROUP BY ac.id_aglomeracaociclo) AS aee ON aee.id = ac.id_aglomeracaociclo
 
@@ -107,9 +111,10 @@ FROM agrocomum.inscricaoestadual AS ie
                              INNER JOIN gta.gta AS gta ON gta.id_gta = gt.id_gta
                              INNER JOIN gta.estratificacao_gta AS gem ON gem.id_gta = gt.id_gta
                              INNER JOIN dsa.estratificacao AS es ON gem.id_estratificacao = es.id
+                             INNER JOIN dsa.especie AS esp ON esp.id = es.id_especie
                     WHERE gt.id_destinovinculado = ac.id_aglomeracaociclo
                       AND gt.tp_destino = 'aglomeracao'
-                      AND es.id_especie IN (3, 4) --CAPRINO/OVINO
+                      AND esp.id_grupo = 10 --CAPRINO/OVINO
                       AND gta.bo_ativo = TRUE
                     GROUP BY ac.id_aglomeracaociclo) AS aeco ON aeco.id = ac.id_aglomeracaociclo
 
